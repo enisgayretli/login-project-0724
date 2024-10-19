b6561d059522f53ca179fd70e09ff477981749c0
Enis Gayretli <enis@botanalytics.co>
2024-10-19 14:27:07 +0300

    Login ve Success sayfasi bitti

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
+import { Switch, Route } from 'react-router-dom';
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Login from './components/Login';
+import Success from './components/Success';
 
 function App() {
 
   return (
     <>
-     <p>Ana Sayfa</p>
+      <Switch>
+        <Route exact path="/">
+          <Login />
+        </Route>
+
+        <Route path="/login">
+          <Login />
+        </Route>
+
+        <Route path="/success">
+          <Success />
+        </Route>
+      </Switch>
     </>
   )
 }
